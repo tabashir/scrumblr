de53be0f9bc766b6a244541268c622c18db14b51
Jez McKinley <jez@mckinley.biz>
2017-01-16 00:21:44 +0000

    JM: adding docker configs. THIS BREAKS non-docker
    * will fix tomorrow but uploading working docker config now

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:4
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+# ARG NODE_ENV
+# ENV NODE_ENV $NODE_ENV
+COPY package.json /usr/src/app/
+RUN npm install && npm cache clean
+COPY . /usr/src/app
+
+EXPOSE 8888
+
+CMD [ "npm", "start" ]
--- base-config.js
+++ base-config.js
@@ -2,21 +2,11 @@ var fs = require('fs');
 
 exports.database = {
 	type: 'redis',
-	prefix: '#scrumblr#'
+	prefix: '#scrumblr#',
+	dbPort: '6379',
+	dbHost: 'redis'
 };
 
-/*
-	Create a conf directory containing a file config.js
-	
-	In here, configure your jira connection details like so:
-	
-	exports.additionalConfig = {
-		jiraUrl: 'https://jira.url/',
-		jiraUser: 'username',
-		jiraPass: 'password'
-}
-
-*/
 
 if (fs.existsSync('./conf/config.js')) {
 	exports.config = require('./conf/config.js').additionalConfig;
